8fe5e12b0f7d98208e8ac667d2d1954de8e970e3
brandon_ruizmora <brandon_ruizmor@outlook.es>
2021-01-13 13:52:57 -0600

    added comments on main

--- src/main/java/com/clashofjava/PokeAPIMania/PokeApiManiaApplication.java
+++ src/main/java/com/clashofjava/PokeAPIMania/PokeApiManiaApplication.java
@@ -3,6 +3,11 @@ package com.clashofjava.PokeAPIMania;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Esta es a clase main
+ *
+ */
+
 @SpringBootApplication
 public class PokeApiManiaApplication {
 

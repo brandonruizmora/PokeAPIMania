ce330cba9657c1b1bd413c79bba2554982b10b8c
brandon_ruizmora <brandon_ruizmor@outlook.es>
2021-01-13 13:30:39 -0600

    first commit

---
+++ src/main/java/com/clashofjava/PokeAPIMania/PokeApiManiaApplication.java
@@ -0,0 +1,13 @@
+package com.clashofjava.PokeAPIMania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiManiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApiManiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clashofjava/PokeAPIMania/PokeApiManiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clashofjava.PokeAPIMania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeApiManiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

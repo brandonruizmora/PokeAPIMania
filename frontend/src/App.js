bdd9ddd287cd482bdeb444c00c70f6ddbf0e5a20
brandon_ruizmora <brandon_ruizmor@outlook.es>
2021-01-14 17:55:42 -0600

    changed layout of mainpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 import MainPage from './components/MainPage';
 import Aboutpage from  './components/Aboutpage';
-import HeaderComponent from './components/HeaderComponent';
 
 function App() {
   return (
-    <div className="App">
-    <HeaderComponent />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
       <MainPage />
-     <Aboutpage/>
     </div>
   );
 }
--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React from "react";
 
 const MainPage = () => {
-    return (
-        <div>
-           <h1>Bienvenidos AMIGOS</h1> 
+  return (
+    <div className="container-fluid fondo">
+      <div className="row alto align-items-center justify-content-center text-center text-light">
+        <div className="col-md-8">
+          <div className="display-1">
+            <i className="fas fa-cog fa-spin"></i>
+          </div>
+          <h1 className="display-4">PokéAPIMania</h1>
+          <p className="lead mb-md-5">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Distinctio, praesentium! Tempore, natus. Cumque nemo ipsa aliquid,
+            cupiditate aut deleniti reprehenderit.
+          </p>
+          <hr className="bg-light" />
+          <button
+            type="button"
+            className="btn btn-outline-light mb-2 mb-sm-0 mx-1"
+            data-bs-toggle="modal"
+            data-bs-target="#home"
+          >
+            Pokémones
+          </button>
+          <button
+            type="button"
+            className="btn btn-outline-light mb-2 mb-sm-0 mx-1"
+            data-bs-toggle="modal"
+            data-bs-target="#nosotros"
+          >
+            Tipos
+          </button>
+          <button
+            type="button"
+            className="btn btn-outline-light mb-2 mb-sm-0 mx-1"
+            data-bs-toggle="modal"
+            data-bs-target="#productos"
+          >
+            Subir
+          </button>
+          <button
+            type="button"
+            className="btn btn-outline-light mb-2 mb-sm-0 mx-1"
+            data-bs-toggle="modal"
+            data-bs-target="#contacto"
+          >
+            Contacto
+          </button>
         </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;

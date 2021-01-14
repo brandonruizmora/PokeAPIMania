ace7fbec2fc42aad596504c1b3a44fdf0d0480f2
brandon_ruizmora <brandon_ruizmor@outlook.es>
2021-01-14 16:07:51 -0600

    added new text

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MainPage = () => {
     return (
         <div>
-           <h1>Bienvenidos</h1> 
+           <h1>Bienvenidos AMIGOS</h1> 
         </div>
     )
 };

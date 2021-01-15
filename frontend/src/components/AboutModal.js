47415b713a47bf1cebaf16b45a08c55c26e0137b
brandon_ruizmora <brandon_ruizmor@outlook.es>
2021-01-14 19:25:46 -0600

    added some fixes

--- frontend/src/components/AboutModal.js
+++ frontend/src/components/AboutModal.js
@@ -26,20 +26,24 @@ const AboutModal = () => {
             </a>
           </div>
           <div className="modal-body">
-            <h1 className="display-1">PokéAPIMania</h1>
-            <p className="lead mb-md-3">
-              The Pokémon Términos de uso Aviso sobre la privacidad Página de
-              Cookies Información legal ©2021 Pokémon
-            </p>
-            <hr className="bg-light" />
-          </div>
-          <div>
-            <h1 className="display-2">PokéAPIMania</h1>
-            <p className="lead mb-md-3">
-              Brandon Ruiz Morales Juan Alejandro Rendon Lucio Erick Osvaldo
-              Carreon Rivera Tere Griselda Valadez Hernandez
-            </p>
-            <hr className="bg-light" />
+
+            <div>
+              <h1 className="display-1">PokéAPIMania</h1>
+              <p className="lead mb-md-3">
+                The Pokémon Términos de uso Aviso sobre la privacidad Página de
+                Cookies Información legal ©2021 Pokémon
+              </p>
+              <hr className="bg-light" />
+            </div>
+            <div>
+              <h1 className="display-2">PokéAPIMania</h1>
+              <p className="lead mb-md-3">
+                Brandon Ruiz Morales Juan Alejandro Rendon Lucio Erick Osvaldo
+                Carreon Rivera Tere Griselda Valadez Hernandez
+              </p>
+              <hr className="bg-light" />
+            </div>
+            
           </div>
           <div className="modal-footer">
             <button

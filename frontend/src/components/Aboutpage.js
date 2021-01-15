5d9965879ca3b19601a456d92bd7afd5c94d7e19
Tere14 <tereyjavierh@gmail.com>
2021-01-14 18:40:56 -0600

    Mas texto en el Aboutpage

--- frontend/src/components/Aboutpage.js
+++ frontend/src/components/Aboutpage.js
@@ -3,9 +3,35 @@ import React from 'react'
 const Aboutpage = () => {
     return (
         <div>
-            hola amigos que tal como le sva 
+        <h1 className="display-1">PokéAPIMania</h1>
+          <p className="lead mb-md-3">
+            The Pokémon 
+        
+            Términos de uso
+            Aviso sobre la privacidad
+            Página de Cookies
+            Información legal
+            ©2021 Pokémon
+
+          </p>
+          <hr className="bg-light" />
+          </div>
+          >
+        <div> 
+          <h1 className="display-2">PokéAPIMania</h1>
+          <p className="lead mb-md-3">
+            Brandon Ruiz Morales
+            Juan Alejandro Rendon Lucio
+            Erick Osvaldo Carreon Rivera
+            Tere Griselda Valadez Hernandez   
+          </p>
+          <hr className="bg-light" />
+           
         </div>
     )
 }
+
+
+
 
 export default Aboutpage

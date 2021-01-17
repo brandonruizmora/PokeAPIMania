237fe0aa118e837418bbafc41648d6a3b1523800
erickcarreon96 <77118297+erickcarreon96@users.noreply.github.com>
2021-01-16 20:18:26 -0600

    new
    commit

--- src/main/java/com/clashofjava/PokeAPIMania/web/controller/PokemonController.java
+++ src/main/java/com/clashofjava/PokeAPIMania/web/controller/PokemonController.java
@@ -4,9 +4,9 @@ import com.clashofjava.PokeAPIMania.domain.Pokemon;
 import com.clashofjava.PokeAPIMania.domain.service.PokemonService;
 import com.clashofjava.PokeAPIMania.persistence.entity.Poqemon;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
